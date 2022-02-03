c6bf929a5db18d8e92fc0a0abb94ee55f4c4a99b
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2022-02-03 20:26:38 +0000

    fix: Add L85

--- compats/compat_cup_weapons/infos/l85a2.hpp
+++ compats/compat_cup_weapons/infos/l85a2.hpp
@@ -1,22 +1,22 @@
-class CUP_L85A2 {
+class CUP_arifle_L85A2 {
 	model = "CUP_L85A2";
 	rail = "None";
 	ugl = "None";
 };
 
-class CUP_L85A2_NG {
+class CUP_arifle_L85A2_NG {
 	model = "CUP_L85A2";
 	rail = "RIS";
 	ugl = "None";
 };
 
-class CUP_L85A2_G {
+class CUP_arifle_L85A2_G {
 	model = "CUP_L85A2";
 	rail = "None";
 	ugl = "FG";
 };
 
-class CUP_L85A2_GL {
+class CUP_arifle_L85A2_GL {
 	model = "CUP_L85A2";
 	rail = "None";
 	ugl = "L123A2";
